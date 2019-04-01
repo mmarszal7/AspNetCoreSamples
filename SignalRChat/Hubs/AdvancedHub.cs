c060af2ee5d3f12446621892a139975843bfc7aa
mmarszal7 <mmarszal7@gmail.com>
2019-04-01 18:56:33 +0200

    Add abilitu to handle incoming messages

--- SignalRChat/Hubs/AdvancedHub.cs
+++ SignalRChat/Hubs/AdvancedHub.cs
@@ -9,18 +9,18 @@ namespace SignalRChat.Hubs
     {
         public void BroadcastMessage(string name, string message)
         {
-            Clients.All.SendAsync(name, message);
+            Clients.All.SendAsync("_BROADCAST_", name, message);
         }
 
         public void Echo(string name, string message)
         {
-            Clients.Client(Context.ConnectionId).SendAsync(name, message + " (echo from server)");
+            Clients.Client(Context.ConnectionId).SendAsync("_BROADCAST_", name, message + " (echo from server)");
         }
 
         public async Task JoinGroup(string name, string groupName)
         {
             var addition = Groups.AddToGroupAsync(Context.ConnectionId, groupName);
-            var sending = Clients.Group(groupName).SendAsync("_SYSTEM_", $"{name} joined {groupName} with connectionId {Context.ConnectionId}");
+            var sending = Clients.Group(groupName).SendAsync("_SYSTEM_", "_SYSTEM_", $"{name} joined {groupName} with connectionId {Context.ConnectionId}");
 
             await Task.WhenAll(addition, sending).ConfigureAwait(false);
         }
@@ -28,30 +28,30 @@ namespace SignalRChat.Hubs
         public async Task LeaveGroup(string name, string groupName)
         {
             var removal = Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
-            var clientSending = Clients.Client(Context.ConnectionId).SendAsync("_SYSTEM_", $"{name} leaved {groupName}");
-            var groupSending = Clients.Group(groupName).SendAsync("_SYSTEM_", $"{name} leaved {groupName}");
+            var clientSending = Clients.Client(Context.ConnectionId).SendAsync("_SYSTEM_", "_SYSTEM_", $"{name} leaved {groupName}");
+            var groupSending = Clients.Group(groupName).SendAsync("_SYSTEM_", "_SYSTEM_", $"{name} leaved {groupName}");
 
             await Task.WhenAll(removal, clientSending, groupSending).ConfigureAwait(false);
         }
 
         public void SendGroup(string name, string groupName, string message)
         {
-            Clients.Group(groupName).SendAsync(name, message);
+            Clients.Group(groupName).SendAsync("ReceiveMessage", name, message);
         }
 
         public void SendGroups(string name, IReadOnlyList<string> groups, string message)
         {
-            Clients.Groups(groups).SendAsync(name, message);
+            Clients.Groups(groups).SendAsync("ReceiveMessage", name, message);
         }
 
         public void SendUser(string name, string userId, string message)
         {
-            Clients.User(userId).SendAsync(name, message);
+            Clients.User(userId).SendAsync("ReceiveMessage", name, message);
         }
 
         public void SendUsers(string name, IReadOnlyList<string> userIds, string message)
         {
-            Clients.Users(userIds).SendAsync(name, message);
+            Clients.Users(userIds).SendAsync("ReceiveMessage", name, message);
         }
     }
 }
