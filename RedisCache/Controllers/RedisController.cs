2f860f6fd1e3766ca7147f7e8375ac58c325fddb
mmarszal7 <mmarszal7@gmail.com>
2019-04-02 19:36:16 +0200

    Add Redis example

---
+++ RedisCache/Controllers/RedisController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using System.Threading.Tasks;
+
+namespace RedisCache.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RedisController : ControllerBase
+    {
+        private readonly IDatabase RedisDatabase;
+
+        public RedisController(IDatabase database)
+        {
+            RedisDatabase = database;
+        }
+
+        [HttpGet("get/{key}")]
+        public Task<RedisValue> Get(string key)
+        {
+            return RedisDatabase.StringGetAsync(key);
+        }
+
+        [HttpGet("set/{key}={value}")]
+        public Task Set(string key, string value)
+        {
+            return RedisDatabase.StringSetAsync(key, value);
+        }
+    }
+}
---
+++ RedisCache/Startup.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+
+namespace RedisCache
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            var connectionMultiplexer = ConnectionMultiplexer.Connect("localhost:6379");
+            services.AddScoped<IDatabase>(_ => connectionMultiplexer.GetDatabase(0));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
