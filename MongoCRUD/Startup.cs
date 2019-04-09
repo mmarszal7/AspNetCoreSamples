67bba8a93ad7ba2b4ceceefd3f752e61762a4fa7
mmarszal7 <mmarszal7@gmail.com>
2019-04-09 22:02:11 +0200

    Switch back to .NET Core 2.1

--- MongoCRUD/Startup.cs
+++ MongoCRUD/Startup.cs
@@ -1,8 +1,10 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using MongoDB.Driver;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace MongoCRUD
 {
@@ -17,31 +19,30 @@ namespace MongoCRUD
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc()
-                .AddNewtonsoftJson();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            //// Swagger
-            //services.AddSwaggerGen(c => c.SwaggerDoc("v1", new Info { Title = "API docs", Version = "v1" }));
+            // Swagger
+            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new Info { Title = "API docs", Version = "v1" }));
+
+            var mongoClient = new MongoClient(Configuration["MongoConnection"]);
+            services.AddScoped<IMongoDatabase>(_ => mongoClient.GetDatabase("demoDatabase"));
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting(routes =>
+            // Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
             {
-                routes.MapControllers();
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API docs");
             });
 
-            //// Swagger
-            //app.UseSwagger();
-            //app.UseSwaggerUI(c =>
-            //{
-            //    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API docs");
-            //});
+            app.UseMvc();
         }
     }
 }
