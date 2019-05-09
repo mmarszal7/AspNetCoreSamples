38e8803329772b00c19d7d29321e0b72bac97b44
mmarszal7 <mmarszal7@gmail.com>
2019-05-09 16:49:03 +0200

    Separate UI and Message Broker  with CQRS layer

---
+++ MessageBrokers/Services/Commands/SendMessageToQueueCommand.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using MessageBrokers.Services;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MessageBrokers.Notifications
+{
+    public class SendMessageToQueueCommand : INotification
+    {
+        public string QueueName { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class SendMessageToQueueCommandHandler : INotificationHandler<SendMessageToQueueCommand>
+    {
+        private readonly RabbitListener MessageBroker;
+
+        public SendMessageToQueueCommandHandler(RabbitListener messageBroker)
+        {
+            MessageBroker = messageBroker;
+        }
+
+        public Task Handle(SendMessageToQueueCommand command, CancellationToken cancellationToken) =>
+            Task.Run(() => MessageBroker.Send(command.QueueName, command.Message));
+    }
+}
---
+++ MessageBrokers/Services/Commands/SubscribeOnQueueCommand.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using MessageBrokers.Services;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MessageBrokers.Notifications
+{
+    public class SubscribeOnQueueCommand : INotification
+    {
+        public string QueueName { get; set; }
+        public Action<string> Callback { get; set; }
+    }
+
+    public class SubscribeOnQueueCommandHandler : INotificationHandler<SubscribeOnQueueCommand>
+    {
+        private readonly RabbitListener MessageBroker;
+
+        public SubscribeOnQueueCommandHandler(RabbitListener messageBroker)
+        {
+            MessageBroker = messageBroker;
+        }
+
+        public Task Handle(SubscribeOnQueueCommand command, CancellationToken cancellationToken) =>
+            Task.Run(() => MessageBroker.Register(command.QueueName, command.Callback));
+    }
+}
